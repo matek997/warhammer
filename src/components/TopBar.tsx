e5b26fe459d6eb3e9fc6fc2fb94b2efceedb80a0
Mateusz Hanzel <mateusz.hanzel@interia.pl>
2020-11-17 15:35:10 +0100

    menu placeholder

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,14 +1,30 @@
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@material-ui/core"
+import { AppBar, Button, Drawer, IconButton, Toolbar, Typography } from "@material-ui/core"
 import React from "react"
 import MenuIcon from '@material-ui/icons/Menu';
+import SignalWifi2Bar from '@material-ui/icons/SignalWifi2Bar'
+import { useState } from "react";
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 export const TopBar = ()=>{
-
+  const [open,setOpen] = useState(false);
 	return       <AppBar position='sticky' >
   <Toolbar>
-    <IconButton   color="inherit" aria-label="menu">
+    <IconButton onClick={()=>{setOpen(true);}}   color="inherit" aria-label="menu">
       <MenuIcon />
     </IconButton>
+    <Drawer anchor='left' open={open} onClose={()=>setOpen(false)}>
+        <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon><SignalWifi2Bar /></ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+    </Drawer>
     <Typography variant="h6">
       Warhammer
     </Typography>
