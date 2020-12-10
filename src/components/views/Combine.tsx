4acf91feaf1c9d85b4630424e42b96028690b42b
mhanzel <mhanzel@LD01148.geco.local>
2020-12-10 15:59:26 +0100

    WiP

--- src/components/views/Combine.tsx
+++ src/components/views/Combine.tsx
@@ -10,6 +10,11 @@ import { useState } from "react";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Button from "@material-ui/core/Button";
+import { IProfession } from "../../models/IProfession";
+import { InputLabel } from "@material-ui/core";
+import { ProfessionProvider } from "../../data/Provider";
+import { ProfessionSelect } from "../ProfessionSelect";
+import { Professions } from "../../data/Professions";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,13 +34,19 @@ const useStyles = makeStyles((theme) => ({
 type ViewState = {
   step: number;
   allowUnsafe: boolean;
+  toCombine: IProfession[];
 };
 
-export const Combine = () => {
-  let [viewState, setViewState] = useState({
-    allowUnsafe: false,
-    step: 0,
-  } as ViewState);
+export const Combine = (props: { fromState?: ViewState }) => {
+  let [viewState, setViewState] = useState(
+    props.fromState ??
+      ({
+        allowUnsafe: false,
+        step: 0,
+        toCombine: [],
+      } as ViewState)
+  );
+  const profsAll = ProfessionProvider.getAll();
   const refreshState = () => setViewState(Object.assign({}, viewState));
   const handleButtonClick = () => {
     console.log(viewState);
@@ -43,7 +54,28 @@ export const Combine = () => {
     refreshState();
   };
   const classes = useStyles();
+  const getValidOptions = () => {
+    if (viewState.allowUnsafe) return profsAll;
 
+    const advanceTo: string[] = [];
+    const current: string[] = [];
+    viewState.toCombine.forEach((el) => {
+      current.push(el.id);
+      el.advanceTo.forEach((id) => {
+        advanceTo.push(id);
+      });
+    });
+    console.log("all");
+    const allowed: { [index: string]: IProfession } = {};
+    Object.keys(profsAll).forEach((id) => {
+      const prof = profsAll[id as Professions];
+      if ((advanceTo.includes(id) || !prof.isAdvanced) && !current.includes(id))
+        allowed[id] = prof;
+    });
+
+    return allowed;
+  };
+  const opts = getValidOptions();
   return (
     <Container>
       <Typography variant="h3" gutterBottom>
@@ -99,6 +131,31 @@ export const Combine = () => {
               </div>
             </Paper>
           </Grid>
+          {viewState.step === 1 && (
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paperFlex}>
+                {viewState.toCombine.length === 0 && (
+                  <div>
+                    <InputLabel>Choose starting class</InputLabel>
+                    <ProfessionSelect
+                      options={opts}
+                      value={Object.keys(opts)[0]}
+                    />
+                  </div>
+                )}
+              </Paper>
+            </Grid>
+          )}
+          {viewState.step === 2 && (
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paperFlex}></Paper>
+            </Grid>
+          )}
+          {viewState.step === 3 && (
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paperFlex}></Paper>
+            </Grid>
+          )}
         </Grid>
       </div>
     </Container>
